6da795fad0aa91dd25c655b5296e4cd5771d74e9
Naveen171819 <naveen.kollipaka@tapinnov.com>
2026-04-09 00:47:16 +0530

    Fix deployment: Update catch-all route for Express 5 compatibility

--- server/index.js
+++ server/index.js
@@ -457,7 +457,7 @@ io.on('connection', (socket) => {
     });
 });
 
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/dist/index.html'));
 });
 
